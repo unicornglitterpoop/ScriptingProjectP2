2cd028e47e12b5c55910e7418c7fac7044f22256
unicornglitterpoop <761368@student.dusd.net>
2024-10-03 09:59:27 -0700

    2nd acripting assignment
    2nd scripting assignment createdd

---
+++ ScriptingProjectP2Payan/Assets/Scripts/Variablesandfunctions.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variablesandfunctions : MonoBehaviour
+{
+    int myInt = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result; 
+    }
+}
+
